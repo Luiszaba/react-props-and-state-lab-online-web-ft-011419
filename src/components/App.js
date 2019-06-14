2ffb2e2cf290990ca9d903328f22aacdb8c1910a
Luis Zabala <luis.zabala076@gmail.com>
2019-06-13 21:49:16 -0400

    understanding fetch

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
+
+  // the objective of this react file is to use the components
+
   constructor() {
     super()
 
@@ -11,8 +14,39 @@ class App extends React.Component {
       pets: [],
       filters: {
         type: 'all'
+        // {this.state.filters.type !== 'all' or === 'all' }
       }
     }
+  }
+
+  fetchPets = () => {
+    // create a fetch function to fetch the API of the pets and set the url being fetched to a mock URL for sake of simplification
+
+    let url = '/api/pets';
+    // !== returns true if the objects are the same type and similar  we are expecting !== to equal 'all' or similar.  Look to line 17 for source
+
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`;
+    }
+    // now we must fetch from the url 
+    fetch(url) 
+    // .then ( response TO .json())
+      .then( res => res.json() )
+      // .then take pets TO this.setState *set the state* ({ pets })
+      .then( pets => this.setState( {pets} ));  
+  };
+
+  // create callback prop to change whenever the value of <select> is changed with the value of the  <select>
+  onChangeType = ({ target: {value} }) => {
+    this.setState({ filters: { ...this.state.filters, type: value }})
+  };
+
+  // create function to find pets via iteration.   create and constant and set it to this.state.pets.  Iterate over the pets with map().  Return all the pets  whose ID matches the pet object.  Its easier to use the spread operator to return truthy objects for every pet object that isAdopted value is set to true
+  onAdoptPets = petID => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petID ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets });
   }
 
   render() {
@@ -23,12 +57,23 @@ class App extends React.Component {
         </header>
         <div className="ui container">
           <div className="ui grid">
+
+
             <div className="four wide column">
               <Filters />
+              onChangeType = { this.onChangeType }
+              fetchPets = {this.fetchPets}
+              
             </div>
+
+
             <div className="twelve wide column">
               <PetBrowser />
+
+
             </div>
+
+
           </div>
         </div>
       </div>
@@ -36,4 +81,4 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default App;
